ceb633715e08070ed84cff02713ad975512b2d41
Roselli <tomasrroselli@gmail.com>
2025-11-18 13:48:08 -0300

    clase abstract Persona

---
+++ src/Persona.java
@@ -0,0 +1,55 @@
+public abstract class Persona
+{
+    //Atributos
+    //Son datos que nunca cambian
+    private final String nombre;
+    private final String apellido;
+    private final String dni;
+    private final String domicilio;
+    private final String origen; //Pais origen
+
+    //Constructor
+    public Persona(String nombre, String apellido, String dni, String domicilio, String origen) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.domicilio = domicilio;
+        this.origen = origen;
+    }
+
+    //Getters
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+
+    @Override
+    public String toString()
+    {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", dni='" + dni + '\'' +
+                ", domicilio='" + domicilio + '\'' +
+                ", origen='" + origen + '\'' +
+                '}';
+    }
+
+
+}
