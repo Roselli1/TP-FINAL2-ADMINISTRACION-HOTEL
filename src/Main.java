39ef1bd5f7ed658187330dae728c6aab4647ef43
Roselli <tomasrroselli@gmail.com>
2025-11-22 01:34:10 -0300

    menu parte del login

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,52 @@
-public class Main {
+import Enums.Rol;
+import Gestores.GestorHotel;
+import Modelo.Persona.UsuarioBase;
+
+import java.util.Scanner;
+
+public class Main
+{
     public static void main(String[] args)
     {
-        
+        //Iniciar el gestor de hotel (carga automatica de datos)
+        GestorHotel hotel= new GestorHotel();
+        Scanner scanner= new Scanner(System.in);
+        int opcion;
+
+
+        System.out.println("--- BIENVENIDO AL SISTEMA DE GESTION HOTELERA ---");
+        do
+        {
+            try
+            {
+                //Solicitar datos de inicio de sesion
+                System.out.println("\n--- INICIO DE SESION ---");
+                System.out.println("Username: ");
+                String username= scanner.nextLine();
+                System.out.println("Password: ");
+                String password= scanner.nextLine();
+
+                UsuarioBase usuarioLogueado= hotel.getUsuarios().get(username);
+
+                if (usuarioLogueado != null && usuarioLogueado.autenticar(username,password))
+                {
+                    System.out.println("Ingreso exitoso. Rol: "+ usuarioLogueado.getRol());
+
+                    //Si es administrador
+                    if (usuarioLogueado.getRol()== Rol.ADMINISTRADOR)
+                    {
+                        //hacer un menun de administrador
+                    }
+                    //Si es recepcionista
+                    else if (usuarioLogueado.getRol()==Rol.RECEPCIONISTA)
+                    {
+                        //menu recepcionista
+                    }
+                }
+            }
+
+
+
+        }while (true);
     }
 }
